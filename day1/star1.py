d5e0d8254bfa561f9dafe32e5fb19599e05e41f5
chenrog <rochenger@gmail.com>
2022-12-15 17:37:22 -0500

    day one

---
+++ day1/star1.py
@@ -0,0 +1,27 @@
+# elves are the index, they have bags of food worth x calories where x is the value
+elves = []
+
+elvesInput = open('input.txt', 'r')
+
+lines = elvesInput.readlines()
+
+bag = 0
+for line in lines:
+  if line == '\n':
+    elves.append(bag)
+    bag = 0
+  else:
+    bag += int(line)
+
+elves.append(bag)
+
+print(elves)
+
+largestBag = 0
+elfToAsk = 0
+for i in range(len(elves)):
+  if elves[i] > largestBag:
+    largestBag = elves[i]
+    elfToAsk = i
+
+print(largestBag)
---
+++ day1/star2.py
@@ -0,0 +1,26 @@
+# elves are the index, they have bags of food worth x calories where x is the value
+elves = []
+
+elvesInput = open('input.txt', 'r')
+
+lines = elvesInput.readlines()
+
+bag = 0
+for line in lines:
+  if line == '\n':
+    elves.append(bag)
+    bag = 0
+  else:
+    bag += int(line)
+
+elves.append(bag)
+
+print(elves)
+
+largestBags = [0, 0, 0]
+for i in range(len(elves)):
+  if elves[i] > largestBags[0]:
+    largestBags = [elves[i], largestBags[1], largestBags[2]]
+    largestBags.sort()
+
+print(sum(largestBags))

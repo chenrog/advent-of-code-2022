b6ae9e51e3e145d9daffc3148ce8bb9de6db533e
chenrog <rochenger@gmail.com>
2022-12-16 09:51:20 -0500

    day4

---
+++ day4/star2.py
@@ -0,0 +1,34 @@
+myInput = open('input.txt', 'r')
+
+lines = myInput.readlines()
+
+class Elf(object):
+
+  def __init__(self, minID, maxID):
+    self.minID = minID
+    self.maxID = maxID
+
+  def fromElfString(elf):
+    elfMin, elfMax = elf.strip().split("-")
+    return Elf(int(elfMin), int(elfMax))
+
+  def isWithin(self, elf):
+    return elf.minID <= self.minID and elf.maxID >= self.maxID
+
+  def overlaps(self, elf):
+    return (self.maxID >= elf.minID and self.maxID <= elf.maxID) or (elf.maxID >= self.minID and elf.maxID <= self.maxID)
+
+  def print(self):
+    print("Elf: " + str(self.minID) + "," + str(self.maxID))
+
+
+overlaps = 0
+for i in range(len(lines)):
+  elf1, elf2 = lines[i].strip().split(",")
+  elf1 = Elf.fromElfString(elf1)
+  elf2 = Elf.fromElfString(elf2)
+
+  if elf1.overlaps(elf2):
+    overlaps += 1
+
+print(overlaps)

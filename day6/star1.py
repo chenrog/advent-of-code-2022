6852b6e1ba4bd5b236b8b495169c9f7422015da3
chenrog <rochenger@gmail.com>
2022-12-17 21:34:35 -0500

    day6 woooooo

---
+++ day6/star1.py
@@ -0,0 +1,21 @@
+myInput = open('input.txt', 'r')
+
+lines = myInput.readlines()
+
+for line in lines:
+  for i in range(len(line)-4):
+    string = line[i:i+4]
+
+    stringDict = {}
+    uniqueString = True
+    for j in range(len(string)):
+      if string[j] in stringDict.keys():
+        uniqueString = False
+        break
+      else:
+        stringDict[string[j]] = 1
+
+    if uniqueString:
+      print(string)
+      print(i + 4)
+      break

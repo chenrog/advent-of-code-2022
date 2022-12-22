cce5438dcec67d73c6a90045da1363b7f1721080
chenrog <rochenger@gmail.com>
2022-12-22 00:56:14 -0500

    update template to allow for index and value of lines by default

--- template_folder/star1.py
+++ template_folder/star1.py
@@ -1,6 +1,6 @@
 with open('test_input.txt', 'r') as f:
   lines = f.readlines()
 
-  for line in lines:
+  for i, line in enumerate(lines):
     line = line.strip()
-    print(line)
+    print(i, line)
